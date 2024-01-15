6542eb8db476bb36c2caebe27043a45292f98cda
gabryel <valvanogabryel@gmail.com>
2024-01-15 10:07:31 -0300

    feat: project base created

---
+++ front-end/src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './routes/Login';
+import SignUp from './routes/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
---
+++ front-end/src/routes/Login.tsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <main className="">
+      <div></div>
+    </main>
+  );
+}
+
+export default Login;
