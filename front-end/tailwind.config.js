6084bf278ff8c0c040189f5fac646a830f77d161
gabryel <valvanogabryel@gmail.com>
2024-01-15 13:07:48 -0300

    feat: implemented the login page

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'keyslab-gray': '#27272A',
+        'keyslab-gray-darker': '#18181B',
+        'keyslab-light-gray': '#A1A1AA',
+        'keyslab-black': '#09090B',
+        'keyslab-blue': '#009EFF',
+        'keyslab-blue-hover': '#005CBF',
+        'light-gray': '#FAFAFA',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        '2.5xl': '1.75rem',
+      },
+      padding: {
+        15: '3.75rem',
+      }
+    },
   },
   plugins: [],
 };
