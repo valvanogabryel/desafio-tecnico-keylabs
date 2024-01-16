5de3b9469848edf8c2c504e123710a2c42d82a37
gabryel <valvanogabryel@gmail.com>
2024-01-15 22:49:31 -0300

    changed api's baseURL

--- front-end/src/api/api.ts
+++ front-end/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4003',
+  baseURL: 'https://keyslab-backend-5b175a71bd82.herokuapp.com',
   withCredentials: true,
 });
 
